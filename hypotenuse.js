d92dc0e268e7d2bf4dc49db5180983248e644547
nuhakhangr8 <nuhakhangr8@gmail.com>
2021-08-27 08:31:40 +0530

    feat: added functions

--- hypotenuse.js
+++ hypotenuse.js
@@ -1,3 +1,20 @@
 const sideInput=document.querySelectorAll(".side-input");
 const hypotenuseButton=document.querySelector("#hypotenuse-button");
 const outputEl=document.querySelector("#output")
+
+function calculateSum(c,s){
+   let a= Number(sideInput[0].value)
+    let b= Number(sideInput[1].value)
+    sum=a*a+b*b;
+    return sum
+}
+
+function calculateHypotenuse (){
+    var sum=calculateSum( Number(sideInput[0].value), Number(sideInput[1].value));
+    var lengthofHypotenuse = Math.sqrt(sum)
+    outputEl.innerText="The length of Hypotenuse is "+lengthofHypotenuse;
+};
+
+
+// Length of the hypotenuse c = sqrt ( a² + b² )
+hypotenuseButton.addEventListener("click",calculateHypotenuse)
